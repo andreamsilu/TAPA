f4d8ac4a58da7cfbba89c47f6105e2c1799cb8e9
andreamsilu <msiluandrew2020@gmail.com>
2023-08-12 10:13:34 +0300

    changes made to the profile page

--- profile.php
+++ profile.php
@@ -283,7 +283,7 @@
                     <div class="row mb-3">
                       <label for="renewPassword" class="col-md-4 col-lg-3 col-form-label">Re-enter New Password</label>
                       <div class="col-md-8 col-lg-9">
-                        <input name="renewpassword" type="password" class="form-control" id="renewPassword">
+                        <input name="renewpassword" type="password" class="form-control" id="renewPassword">  
                       </div>
                     </div>
 
