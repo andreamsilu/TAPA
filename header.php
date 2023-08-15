9ab935f3d7d619a15fd87dc3a65f1de2d6f3b9bd
andreamsilu <msiluandrew2020@gmail.com>
2023-08-15 23:09:39 +0300

    modified

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 
           <li><a href="membership.php">Membership</a></li>
           <li><a href="publication-resources.php">Publication & resources</a></li>
-          <li><a href="portfolio.php">Photo gallery</a></li>
+          <li><a href="photo-gallery.php">Photo gallery</a></li>
           <!-- <li><a href="blog.php">Blog</a></li> -->
           <li><a href="contact.php">Contact</a></li>
 
