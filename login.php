e573a63578fe4d7a3a2e1576fe4467ce2a62543d
andreamsilu <msiluandrew2020@gmail.com>
2023-08-22 20:11:35 +0300

    login page modified

--- login.php
+++ login.php
@@ -1,4 +1,4 @@
-<?php include "header.php" ?>
+<?php //include "header.php" ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -7,31 +7,11 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title> Login -TAPA</title>
+  <title>Member Login</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
-  <!-- Favicons -->
-  <!-- Favicons -->
- <link href="assets/img/tapa.png" rel="icon">
-  <link href="assets/img/tapa.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
-  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
-
+<?php include "titleIcon.php" ?>
  
 </head>
 
@@ -40,17 +20,32 @@
 
 
   <main>
-    <div class="container">
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
 
-      <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>Login</h2>
+          <ol>
+            <li><a href="index.php">Home</a></li>
+            <li>Member login</li>
+          </ol>
+        </div>
+
+      </div>
+    </section>
+    <!-- End Breadcrumbs -->
+    <div class="container bg-primary-subtle">
+
+      <section class="section register min-vh-00 d-flex flex-column align-items-center justify-content-center py-4">
         <div class="container">
           <div class="row justify-content-center">
             <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
 
-              <div class="d-flex justify-content-center py-4">
+              <div class="d-flex justify-content-center py-2">
                 <a href="index.php" class="logo d-flex align-items-center w-auto">
-                  <img src="assets/img/logo.png" alt="">
-                  <span class="d-none d-lg-block">TAPA</span>
+                  <!-- <img src="assets/img/tapa.png" alt="TAPA"> -->
+                  <!-- <span class="d-none d-lg-block">TAPA</span> -->
                 </a>
               </div>
               <!-- End Logo -->
@@ -61,7 +56,7 @@
 
                   <div class="pt-4 pb-2">
                     <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
-                    <p class="text-center small">Enter your username & password to login</p>
+                    <!-- <p class="text-center small">Enter your username & password to login</p> -->
                   </div>
 
                   <form class="row g-3 needs-validation" novalidate>
