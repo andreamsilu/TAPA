55640b0d1042d967fb54de721a16253c2155c2d9
andreamsilu <msiluandrew2020@gmail.com>
2023-08-24 13:44:24 +0300

    profile page responsiveness added

--- profile.php
+++ profile.php
@@ -1,3 +1,10 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
 <?php  include "titleIcon.php" ?>
 <body class="m-3 bg-gradient">
     
