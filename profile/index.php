0196fe8213c3ec2ee54daf7c90d80754f7260f0c
andreamsilu <msiluandrew2020@gmail.com>
2024-03-06 07:24:50 +0300

    profile/index email check

--- profile/index.php
+++ profile/index.php
@@ -5,7 +5,7 @@
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
 
-include '../forms/connection.php';
+include('../forms/connection.php');
 
 // Check if the user is authenticated
 if (!isset($_SESSION['email'])) {
