4ace835119c7ae229df3843022ce67c913be91cb
andreamsilu <msiluandrew2020@gmail.com>
2024-03-12 22:02:39 +0300

    mobile navigation fixing

--- header.php
+++ header.php
@@ -1,57 +1,93 @@
 <head> 
-<?php include "titleIcon.php"; ?>
+    <?php include "titleIcon.php"; ?>
 
-<!-- Bootstrap CSS -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 
-<!-- Bootstrap Icons -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.19.0/font/bootstrap-icons.css">
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.19.0/font/bootstrap-icons.css">
 
-<!-- Header -->
-<header id="header" class="fixed-top">
-  <div class="container d-flex align-items-right">
+    <!-- Header -->
+    <header id="header" class="fixed-top">
+        <div class="container d-flex align-items-center justify-content-between">
+            <a href="index.php">
+                <img src="assets/img/tapa.png" alt="" class="img-fluid logo" width="400px" style="margin-right:1px;">
+            </a>
 
-    <a href="index.php" class="me-auto m-lg-0">
-      <img src="assets/img/tapa.png" alt="" class="img-fluid logo" width="400px" style="margin-right:1px;">
-    </a>
+            <!-- Desktop Navigation -->
+            <nav id="navbar" class="navbar order-last order-lg-0">
+                <ul class="desktop-nav">
+                    <li><a class="" href="index.php">Home</a></li>
+                    <li><a href="about.php">About us</a></li>
+                    <li class="dropdown">
+                        <a href="membeship-category.php">
+                            <span>Membership</span> <i class="bi bi-chevron-down"></i>
+                        </a>
+                        <ul>
+                            <li><a href="login.php">Login</a></li>
+                            <li><a href="membeship-category.php">Register</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="publication-resources.php">Publication & Resources</a></li>
+                    <li><a href="photo-gallery.php">Photo Gallery</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="membeship-category.php">Register</a></li>
+                </ul>
+            </nav>
 
-    <nav id="navbar" class="navbar order-last order-lg-0">
-      <ul class="nav-list">
-        <li><a class="" href="index.php">Home</a></li>
-        <li><a href="about.php">About us</a></li>
+            <!-- Mobile Navigation Toggle -->
+            <i class="bi bi-list mobile-nav-toggle"></i>
+        </div>
+    </header>
 
-        <li class="dropdown">
-          <a href="membeship-category.php">
-            <span>Membership</span> <i class="bi bi-chevron-down"></i>
-          </a>
-          <ul>
+    <!-- Mobile Navigation -->
+    <nav id="mobile-nav" class="mobile-nav">
+        <ul class="nav-list">
+            <li><a class="" href="index.php">Home</a></li>
+            <li><a href="about.php">About us</a></li>
+            <li class="dropdown">
+                <a href="membeship-category.php">
+                    <span>Membership</span> <i class="bi bi-chevron-down"></i>
+                </a>
+                <ul>
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="membeship-category.php">Register</a></li>
+                </ul>
+            </li>
+            <li><a href="publication-resources.php">Publication & Resources</a></li>
+            <li><a href="photo-gallery.php">Photo Gallery</a></li>
+            <li><a href="contact.php">Contact</a></li>
             <li><a href="login.php">Login</a></li>
             <li><a href="membeship-category.php">Register</a></li>
-          </ul>
-        </li>
-       
-        </li> 
-        <li><a href="publication-resources.php">Publication & Resources</a></li>
-        <li><a href="photo-gallery.php">Photo Gallery</a></li>
-        <li><a href="contact.php">Contact</a></li>
-      </ul>
-      <i class="bi bi-list mobile-nav-toggle"></i>
+        </ul>
     </nav>
 
-    <div class="d-flex nav-links">
-      <a href="login.php" class="nav-link">Login/</a>
-      <a href="membeship-category.php" class="nav-link">Register</a>
-    </div>
-  </div>
-</header>
+    <!-- Mobile Navigation Script -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $(".mobile-nav-toggle").click(function () {
+                $("#mobile-nav").toggleClass("show");
+            });
+        });
+    </script>
 
-<!-- Mobile Navigation Script -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<script>
-  $(document).ready(function () {
-    $(".mobile-nav-toggle").click(function () {
-      $("#navbar ul.nav-list").toggleClass("show");
-    });
-  });
-</script>
+    <!-- Mobile Navigation CSS -->
+    <style>
+        /* Hide mobile nav by default */
+        #mobile-nav {
+            display: none;
+        }
+
+        /* Media query to show mobile nav only on small screens */
+        @media (max-width: 767px) {
+            #mobile-nav {
+                display: block;
+            }
+            .desktop-nav {
+                display: none;
+            }
+        }
+    </style>
+</head>
