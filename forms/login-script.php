fa28b42f51f3bf1485cc441299c6eeddc712e986
andreamsilu <msiluandrew2020@gmail.com>
2024-04-04 11:06:22 +0300

    login with payment status

--- forms/login-script.php
+++ forms/login-script.php
@@ -1,17 +1,15 @@
-<?php
-
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-session_start(); // Uncommented session_start()
+session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-  include('../forms/connection.php');
+    include('../forms/connection.php');
 
-    // Query to fetch user details based on the provided email
+    // Query to fetch user details based on the provided email and pay_status condition
     $sql = "SELECT * FROM users WHERE email = '$email' LIMIT 1";
     $result = $conn->query($sql);
 
@@ -19,30 +17,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $row = $result->fetch_assoc();
         $hashedPassword = $row['password'];
 
-        // Verify the provided password with the hashed password from the database
-        if (password_verify($password, $hashedPassword)) {
-            // Password matches, create a session and store user information
-            $_SESSION['user_id'] = $row['id'];
-            $_SESSION['email'] = $email;
-            
-            // Check user role
-            if ($row['role'] == 1) {
-                // Admin user
-                header("Location: ../Admin/news/index.php"); // Redirect to admin dashboard
-            } else {
-                // Normal user
-                header("Location: ../profile/index.php"); // Redirect to user profile
-            }
-            exit();
-        } else {
-            // Incorrect password
-            echo $error = "Invalid credentials. Please try again.";
-            header("Location: ../login.php"); // Redirect to user profile
+        if ($row['pay_status'] == 1) {
+            // Verify the provided password with the hashed password from the database
+            if (password_verify($password, $hashedPassword)) {
+                $_SESSION['user_id'] = $row['id'];
+                $_SESSION['email'] = $email;
 
+                if ($row['role'] == 1) {
+                    header("Location: ../Admin/news/index.php");
+                } else {
+                    header("Location: ../profile/index.php");
+                }
+                exit();
+            } else {
+                echo $error = "Invalid credentials. Please try again.";
+                header("Location: ../login.php");
+            }
+        } else {
+            echo $error = "Please pay the annual fees or renew your membership to continue.";
+            header("Location: ../login.php");
         }
     } else {
-        // User not found
         echo $error = "User with this email does not exist.";
+        header("Location: ../login.php");
     }
 
     $conn->close();
