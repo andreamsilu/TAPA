8f229ae7a688dc4461ea3ef80cbb3520f429b44c
andreamsilu <msiluandrew2020@gmail.com>
2024-04-13 17:38:56 +0300

    added agm section

--- index.php
+++ index.php
@@ -115,7 +115,7 @@ include "header.php";
                 Email: info@tapa.or.tz<br> 
                 WhatsApp: +255719911575<br>
                 Call: +255742670113</p> -->
-            <a href="https://forms.gle/DM5NMKcMRwEJS7UP9" target="_blank class="btn btn-primary" style="background-color:#0F718A ;">Register Now</a>
+            <a href="https://forms.gle/DM5NMKcMRwEJS7UP9" target="_blank" class="btn btn-primary" style="background-color:#0F718A ;">Register Now</a>
         </div>
     </div>
     <div class="card">
