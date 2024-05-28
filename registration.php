d1041ba0b605d41ea96d7698f31bcb9c5bc6c31e
andreamsilu <msiluandrew2020@gmail.com>
2024-05-28 19:22:45 +0300

    modified date field

--- registration.php
+++ registration.php
@@ -5,30 +5,19 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Register</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
+
   <!-- Favicons -->
   <link href="assets/img/tapa.png" rel="icon">
   <link href="assets/img/tapa.png" rel="apple-touch-icon">
-  <!-- Google Fonts -->
-  <!-- <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet"> -->
-  <!-- Vendor CSS Files -->
-  <!-- <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
-  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet"> -->
-  <!-- Template Main CSS File -->
-  <!-- <link href="test1.css" rel="stylesheet"> -->
-  <!-- <link href="assets/css/style.css" rel="stylesheet"> -->
 
+  <!-- Bootstrap CSS and Flatpickr CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+
+  <!-- Google reCAPTCHA -->
   <script src="https://www.google.com/recaptcha/enterprise.js?render=6LfB6aEpAAAAAAqhOtkcweZgJDsXn3kV-FabTfep"></script>
-  <!-- Your code -->
-
 </head>
+
 <style>
   /* Registration Section */
   .registration {
@@ -36,8 +25,6 @@
     background-color: #eee;
     font-family: 'Roboto', Sans-serif;
   }
-
-
 
   .registration h1 {
     font-size: 36px;
@@ -53,7 +40,6 @@
     border-radius: 8px;
     padding: 10px 20px;
     font-size: 18px;
-    cursor: auto;
     transition: background-color 0.3s;
   }
 
@@ -61,8 +47,6 @@
     color: #0F718A;
     font-weight: 600;
   }
-
-
 
   .registration button[type="submit"] {
     color: #0F718A;
@@ -81,13 +65,7 @@
   }
 
   .form-control:hover {
-    color: #000;
     border: #0c5b6d solid 2px;
-  }
-
-
-  .registration placeholder:hover {
-    color: #eee;
   }
 
   .registration .form-group {
@@ -98,39 +76,15 @@
     color: #0F718A;
   }
 
-
-
-  .registration p {
-    color: #000;
-  }
-
   .registration a:hover {
     text-decoration: underline;
     color: #0c5b6d;
   }
-
-  .btn-terms {
-    color: #0c5b6d;
-    border: solid #0c5b6d 2px;
-    border-radius: 5px;
-
-  }
-
-  .btn-terms button[type=button]:hover {
-    color: #eee;
-    border: solid #0c5b6d 2px;
-    border-radius: 5px;
-    background-color: #0F718A;
-  }
 </style>
 
 <body>
-  <?php
-  include "titleIcon.php";
-  include "header.php"
-
-
-  ?>
+  <?php include "titleIcon.php"; ?>
+  <?php include "header.php"; ?>
 
   <main>
     <!-- Breadcrumbs -->
@@ -147,17 +101,17 @@
     </section>
     <!-- End Breadcrumbs -->
 
-    <!-- ======= registration Section ======= -->
-    <section id="ragistration" class="registration">
+    <!-- Registration Section -->
+    <section id="registration" class="registration">
       <div class="container mt-5">
         <div class="row justify-content-center">
           <div class="col-md-12">
             <div class="card">
               <div class="card-header">
-                <h2 class="text-center"><i class="bi bi-person"></i>Membership Registration Form</h2>
+                <h2 class="text-center"><i class="bi bi-person"></i> Membership Registration Form</h2>
               </div>
               <div class="card-body">
-                <form action="forms/register-script.php" method="post" role="form" enctype="multipart/form-data">
+                <form id="myForm" action="forms/register-script.php" method="post" role="form" enctype="multipart/form-data">
                   <div class="row">
                     <div class="col-md-6">
                       <div class="form-group">
@@ -171,7 +125,7 @@
                       </div>
                       <div class="form-group">
                         <label for="phone"><i class="bi bi-telephone"></i> Phone</label>
-                        <input type="phone" class="form-control" name="phone" id="phone" placeholder="Your phone" required>
+                        <input type="text" class="form-control" name="phone" id="phone" placeholder="Your phone" required>
                       </div>
                       <div class="form-group">
                         <label for="postal_address"><i class="bi bi-house-door"></i> Postal Address</label>
@@ -179,7 +133,13 @@
                       </div>
                       <div class="form-group">
                         <label for="birth_date"><i class="bi bi-calendar"></i> Birth date</label>
-                        <input type="date" class="form-control" name="birth_date" id="birth_date" placeholder="Birthdate" required>
+                        <input type="text" class="form-control" name="birth_date" id="birth_date" placeholder="Birthdate" required>
+                      </div>
+
+                      <div class="form-group">
+                        <label for="cv"><i class="bi bi-file-earmark-pdf"></i> Upload your CV</label>
+                        <input type="file" class="form-control" name="cv" id="cv" placeholder="Upload your cv" accept=".pdf,.doc,.docx" required>
+                        <small class="text-muted">Accepted file formats: PDF, DOC, DOCX</small>
                       </div>
                     </div>
                     <div class="col-md-6">
@@ -191,10 +151,10 @@
                         <label for="membership_type">Select Membership category</label>
                         <select class="form-control" id="membership_type" name="membership_type" required>
                           <option value="" disabled selected>Select membership category</option>
-                          <option value="student">Full Member</option>
+                          <option value="full_member">Full Member</option>
                           <option value="associate_one">Associate Member I</option>
                           <option value="associate_two">Associate Member II</option>
-                          <option value="student">Student Member </option>
+                          <option value="student">Student Member</option>
                           <option value="local_affiliate">Local Affiliate Member</option>
                           <option value="foreign_affiliate">Foreign Affiliate Member</option>
                         </select>
@@ -231,34 +191,23 @@
                       </div>
                     </div>
                   </div>
-                  <div class="form-group">
+                  <!-- <div class="form-group">
                     <label for="cv"><i class="bi bi-file-earmark-pdf"></i> Upload your CV</label>
                     <input type="file" class="form-control" name="cv" id="cv" placeholder="Upload your cv" accept=".pdf,.doc,.docx" required>
                     <small class="text-muted">Accepted file formats: PDF, DOC, DOCX</small>
-                  </div>
-                  <!-- <div class="form-group">
-                            <label for="password"><i class="bi bi-lock"></i> Password</label>
-                            <input type="password" class="form-control" name="password" id="password" placeholder="Your password" required minlength="8">
-                            <small class="text-muted">Minimum length: 8 characters</small>
-                        </div>
-                        <div class="form-group">
-                            <label for="confirm-password"><i class="bi bi-lock"></i> Confirm password</label>
-                            <input type="password" class="form-control" name="confirm-password" id="confirm-password" placeholder="Confirm password" required minlength="8">
-                        </div> -->
+                  </div> -->
                   <div class="form-group">
-                    <div class="g-recaptcha" data-sitekey="6LfB6aEpAAAAAAqhOtkcweZgJDsXn3kV-FabTfep"></div>
-                  </div>
-                  <div class="form-group">
-                    <button type="button" class="btn btn-secondary btn-block" data-toggle="modal" data-target="#termsModal">
+                    <button type="button" class="btn btn-secondary accordion btn-block" data-toggle="modal" data-target="#termsModal">
                       <i class="bi bi-file-earmark-text"></i> Read the terms and conditions
                     </button>
                   </div>
                   <div class="form-check">
-                    <input type="checkbox" class="form-check-input" name="crime_yes" value="yes" id="agree_terms" required>
-                    <label class="form-check-label" for="agree_terms"><i class="bi bi-check-square"></i> I agree to the terms and conditions</label>
+                    <input type="checkbox" class="form-check-input" id="termsCheck" name="termsCheck" required>
+                    <label class="form-check-label" for="termsCheck">I agree to the terms and conditions</label>
                   </div>
-                  <button type="submit" class="btn  btn-block"><i class="bi bi-person-plus"></i> Register</button>
-                  <p class="large mt-3" style="text-align: center; font-size: large;">Already have an account? <a href="login.php"><i class="bi bi-box-arrow-in-right"></i> Login Now</a></p>
+                  <div class="text-center mt-4">
+                    <button type="submit" class="btn btn-primar">Register</button>
+                  </div>
                 </form>
               </div>
             </div>
@@ -266,96 +215,30 @@
         </div>
       </div>
     </section>
-    <!-- End registration Section -->
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-    <!-- End Profile registration Form -->
+    <!-- End Registration Section -->
   </main>
 
+  <?php include "footer.php"; ?>
 
-  <!-- Modal -->
-  <div class="modal fade" id="termsModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Terms and Conditions</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <p></strong>
-            Welcome to the Tanzania Association of Psychologists (TAPA) website. By accessing and using
-            this website, you accept and agree to comply with the following terms and conditions: <br>
-
-            <strong>Website Usage:</strong> The content provided on this website is for informational purposes only. It should not be considered as professional advice or a substitute for professional consultation with qualified psychologists or mental health professionals. <br>
-
-            <strong>Accuracy of Information: </strong>We strive to ensure the accuracy and reliability of the information presented on this website. However, we do not warrant the completeness, correctness, or suitability of the information for any purpose. <br>
-
-            <strong>User Responsibilities:</strong> Users of this website are responsible for their actions and the use of information obtained from the site. TAPA shall not be held liable for any direct or indirect damages resulting from the use or misuse of information provided herein. <br>
-
-            <strong>Intellectual Property: </strong>All content, including text, images, logos, and graphics on this website, is the property of TAPA and is protected by intellectual property laws. Unauthorized use, reproduction, or distribution of any content is strictly prohibited without prior written consent. <br>
-
-            <strong> Links to Third-Party Websites:</strong> This website may contain links to external websites for additional information. TAPA does not endorse or take responsibility for the content, accuracy, or security of these third-party websites. <br>
-
-            <strong> Privacy Policy:</strong> Your privacy is important to us. Please refer to our Privacy Policy for information on how we collect, use, and protect your personal data. <br>
-
-            <strong> Modification of Terms:</strong> TAPA reserves the right to modify or update these terms and conditions at any time without prior notice. Users are encouraged to review these terms periodically for any changes. <br>
-
-            <strong>Governing Law: </strong>These terms and conditions shall be governed by and construed in accordance with the laws of Tanzania. Any disputes arising from the use of this website shall be subject to the jurisdiction of Tanzanian courts. <br>
-
-
-            By accessing and using the TAPA website, you agree to abide by these terms and conditions. If you do not agree with any part of these terms, please refrain from using this website. <br>
-
-            For any inquiries or concerns regarding these terms and conditions, please contact us at <strong>info@tapa.or.tz</strong>
-          </p>
-          <!-- Include the terms and conditions content from the previous example -->
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <?php include "footer.php" ?>
+  <!-- Bootstrap and Flatpickr JavaScript -->
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+  <script>
+    flatpickr("#birth_date", {
+      dateFormat: "Y-m-d"
+    });
+
+    grecaptcha.ready(function() {
+      grecaptcha.execute('6LfB6aEpAAAAAAqhOtkcweZgJDsXn3kV-FabTfep', {
+        action: 'submit'
+      }).then(function(token) {
+        var recaptchaResponse = document.getElementById('recaptchaResponse');
+        recaptchaResponse.value = token;
+      });
+    });
+  </script>
 </body>
 
 </html>
-<script>
-  function onClick(e) {
-    e.preventDefault();
-    grecaptcha.enterprise.ready(async () => {
-      const token = await grecaptcha.enterprise.execute('6LfB6aEpAAAAAAqhOtkcweZgJDsXn3kV-FabTfep', {
-        action: 'LOGIN'
-      });
-    });
-  }
-
-  // Function to validate full name
-  function validateFullName() {
-    var fullnameInput = document.getElementById('fullname');
-    var fullname = fullnameInput.value.trim();
-
-    // Regular expression to match only alphabetical characters
-    var regex = /^[a-zA-Z ]+$/;
-
-    if (!regex.test(fullname)) {
-      document.getElementById('nameError').textContent = 'Please enter only alphabetical characters';
-      fullnameInput.focus();
-      return false;
-    } else {
-      document.getElementById('nameError').textContent = '';
-      return true;
-    }
-  }
-
-  // Form submission validation
-  document.getElementById('myForm').addEventListener('submit', function(event) {
-    if (!validateFullName()) {
-      event.preventDefault(); // Prevent form submission if validation fails
-    }
-  });
-</script>
