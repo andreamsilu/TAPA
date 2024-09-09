71559ae184a02085149e1554e4533bae5754cf1f
andreamsilu <msiluandrew2020@gmail.com>
2024-09-09 19:51:26 +0300

    member list modification

--- Admin/news/member_list.php
+++ Admin/news/member_list.php
@@ -32,6 +32,16 @@ if (!isset($_SESSION['email'])) {
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.html5.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/1.7.1/js/buttons.print.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+
+<!-- Additional custom CSS to make action buttons smaller -->
+<style>
+.btn-sm {
+    padding: 0.25rem 0.5rem;
+    font-size: 0.75rem;
+}
+</style>
 
 <script>
 $(document).ready(function() {
@@ -75,7 +85,7 @@ $(document).ready(function() {
         ],
         pageLength: 10, // Pagination size
         lengthMenu: [10, 25, 50, 75, 100], // Pagination options
-        dom: 'Bfrtip',
+        dom: 'Bfrtip', // Show the buttons on top
         buttons: [{
                 extend: 'excelHtml5',
                 text: 'Export Excel',
@@ -85,6 +95,11 @@ $(document).ready(function() {
                 extend: 'pdfHtml5',
                 text: 'Export PDF',
                 className: 'btn btn-sm btn-danger'
+            },
+            {
+                extend: 'print',
+                text: 'Print',
+                className: 'btn btn-sm btn-info'
             }
         ]
     });
