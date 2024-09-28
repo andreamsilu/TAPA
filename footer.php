daad91c563b75dc1d3dfd9d356870e1e24529e4a
andreamsilu <msiluandrew2020@gmail.com>
2024-09-28 14:29:15 +0300

    membershp system modification

--- footer.php
+++ footer.php
@@ -2,86 +2,93 @@
 
 <footer id="footer">
 
-<div class="footer-top">
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-3 col-md-6 footer-contact">
-        <h3>TAPA</h3>
-        <p>
-        Esaurp Village, Near Mlimani City <br>
-          P.O.BOX 35417,<br> Dar es Salaam<br>
-          Tanzania <br><br>
-          <strong>Phone:</strong> +255 719 911 575<br>
-          <strong>Email:</strong> info@tapa.or.tz<br>
-        </p>
-      </div>
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 footer-contact">
+                    <h3>TAPA</h3>
+                    <p>
+                        Esaurp Village, Near Mlimani City <br>
+                        P.O.BOX 35417,<br> Dar es Salaam<br>
+                        Tanzania <br><br>
+                        <strong>Phone:</strong> +255 719 911 575<br>
+                        <strong>Email:</strong> info@tapa.or.tz<br>
+                    </p>
+                </div>
 
-      <div class="col-lg-2 col-md-6 footer-links">
-        <h4>Useful Links</h4>
-        <ul>
-          <li><i class="bx bx-chevron-right"></i> <a href="index.php">Home</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="about.php">About us</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="membership.php">Membership</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="publication-resources.php">Publication</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="portifolio.php">Photo & gallery</a></li>
-        </ul>
-      </div>
+                <div class="col-lg-2 col-md-6 footer-links">
+                    <h4>Useful Links</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="index.php">Home</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="about.php">About us</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="membership.php">Membership</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="publication-resources.php">Publication</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="portifolio.php">Photo & gallery</a></li>
+                    </ul>
+                </div>
 
-      <div class="col-lg-3 col-md-6 footer-links">
-        <h4>TAPA activities</h4>
-        <ul>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Advocacy</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Research</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Ethics and Standards</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Continuing Education </a></li>
-        </ul>
-      </div>
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>TAPA activities</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Advocacy</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Research</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Ethics and Standards</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Continuing Education </a></li>
+                    </ul>
+                </div>
 
-      <div class="col-lg-4 col-md-6 footer-newsletter">
-        <h4>Join Our Newsletter</h4>
-        <p>Stay up-to-date with our latest news and updates by subscribing to our newsletter.</p>
-        <form action="" method="post">
-          <input type="email" name="email"><input type="submit" value="Subscribe">
-        </form>
-      </div>
-    <div class="  social-links ml-3 me-md-auto text-center text-md-start">
-    <a href="https://x.com/tapa_tz?t=Xa40Dj9GacpBmaTJ863zqA&s=08" class="twitter"><i class="bx bxl-twitter"></i></a>
-        <a href="https://www.facebook.com/profile.php?id=100092108256995" class="facebook"><i class="bx bxl-facebook"></i></a>
-        <a href="https://instagram.com/tapa_tz?igshid=MzRlODBiNWFlZA==" class="instagram"><i class="bx bxl-instagram"></i></a>
-        <a href="https://www.linkedin.com/in/tanzanian-psychological-association-2b83a2287" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                <div class="col-lg-4 col-md-6 footer-newsletter">
+                    <h4>Join Our Newsletter</h4>
+                    <p>Stay up-to-date with our latest news and updates by subscribing to our newsletter.</p>
+                    <form action="" method="post">
+                        <input type="email" name="email"><input type="submit" value="Subscribe">
+                    </form>
+                </div>
+                <div class="  social-links ml-3 me-md-auto text-center text-md-start">
+                    <a href="https://x.com/tapa_tz?t=Xa40Dj9GacpBmaTJ863zqA&s=08" class="twitter"><i
+                            class="bx bxl-twitter"></i></a>
+                    <a href="https://www.facebook.com/profile.php?id=100092108256995" class="facebook"><i
+                            class="bx bxl-facebook"></i></a>
+                    <a href="https://instagram.com/tapa_tz?igshid=MzRlODBiNWFlZA==" class="instagram"><i
+                            class="bx bxl-instagram"></i></a>
+                    <a href="https://www.linkedin.com/in/tanzanian-psychological-association-2b83a2287"
+                        class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                    <a href="https://www.linkedin.com/in/tanzanian-psychological-association-2b83a2287"
+                        class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-  </div>
-</div>
-</div>
+    </div>
 
-<div class="container d-md-flex py-4">
+    <div class="container d-md-flex py-4">
 
-   <div class="social-links  me-md-auto text-center text-md-start">
-  <!-- <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <div class="social-links  me-md-auto text-center text-md-start">
+            <!-- <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
     <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
     <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
     <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
     <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a> -->
-  </div> 
+        </div>
 
-  <div class="me-md-auto  text-center text-md-right pt-3 pt-md-0">
-  <div class="copyright">
-      &copy;  <strong><span>TAPA</span></strong>. All Rights Reserved
+        <div class="me-md-auto  text-center text-md-right pt-3 pt-md-0">
+            <div class="copyright">
+                &copy; <strong><span><a href="tapanew/index.php">TAPA</a></span></strong>. All Rights Reserved
+            </div>
+            <div class="credits">
+                <!-- All the links in the footer should remain intact. -->
+                <!-- You can delete the links only if you purchased the pro version. -->
+                <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/TAPA-free-html-bootstrap-template/ -->
+                Designed by <a href="https://github.com/andreamsilu/andreamsilu.io">Andrea Msilu</a>
+            </div>
+        </div>
     </div>
-    <div class="credits">
-      <!-- All the links in the footer should remain intact. -->
-      <!-- You can delete the links only if you purchased the pro version. -->
-      <!-- Licensing information: https://bootstrapmade.com/license/ -->
-      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/TAPA-free-html-bootstrap-template/ -->
-      <!-- Designed by <a href="https://github.com/andreamsilu/andreamsilu.io">Andrea Msilu</a> -->
-    </div>
-  </div>
-</div>
 </footer>
 <!-- End Footer -->
 
-<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+        class="bi bi-arrow-up-short"></i></a>
 
 <!-- Vendor JS Files -->
 <script src="assets/vendor/aos/aos.js"></script>
@@ -96,4 +103,5 @@
 <script src="assets/js/main.js"></script>
 
 </body>
+
 </html>
