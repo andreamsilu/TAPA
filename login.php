1436960fb0a1cefefdec4e2efd412b384002bf7b
andreamsilu <msiluandrew2020@gmail.com>
2024-11-16 10:30:49 +0300

    login page

--- login.php
+++ login.php
@@ -9,14 +9,17 @@ include('header.php');
 <body>
     <section class="ftco-section">
         <div class="container">
-            <div class="row justify-content-center">
+            <!-- <div class="row justify-content-center">
                 <div class="col-md-6 text-center mb-1 mt-5">
                     <div class="section-title">
                         <h2 class="pt-1">SIGN IN WITH YOUR TAPA ACCOUNT</h2>
                     </div>
                 </div>
-            </div>
+            </div> -->
             <div class="row justify-content-center">
+                <div class="section-title">
+                    <h2 class="pt-1">SIGN IN WITH YOUR TAPA ACCOUNT</h2>
+                </div>
                 <div class="col-md-12 col-lg-10">
                     <div class="wrap d-md-flex">
                         <div class="img" style="background-image: url(assets/img/tapa/tapa-fam.JPG);">
@@ -28,28 +31,36 @@ include('header.php');
                                 </div>
                                 <div class="w-100">
                                     <p class="social-media d-flex justify-content-end">
-                                        <a href="https://www.facebook.com/profile.php?id=100092108256995" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-facebook"></span></a>
-                                        <a href="https://x.com/tapa_tz?t=Xa40Dj9GacpBmaTJ863zqA&s=08" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-twitter"></span></a>
+                                        <a href="https://www.facebook.com/profile.php?id=100092108256995"
+                                            class="social-icon d-flex align-items-center justify-content-center"><span
+                                                class="fa fa-facebook"></span></a>
+                                        <a href="https://x.com/tapa_tz?t=Xa40Dj9GacpBmaTJ863zqA&s=08"
+                                            class="social-icon d-flex align-items-center justify-content-center"><span
+                                                class="fa fa-twitter"></span></a>
                                     </p>
                                 </div>
                             </div>
                             <form action="forms/login-script.php" class="signin-form" method="post">
                                 <div class="form-group mb-3">
                                     <label class="label" for="email">Email</label>
-                                    <input type="email" class="form-control" name="email" placeholder="example@gmail.com" required>
+                                    <input type="email" class="form-control" name="email"
+                                        placeholder="example@gmail.com" required>
                                 </div>
                                 <div class="form-group mb-3">
                                     <label class="label" for="password">Password</label>
                                     <div class="input-group">
-                                        <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
+                                        <input type="password" class="form-control" id="password" name="password"
+                                            placeholder="Password" required>
                                         <div class="input-group-append">
-                                            <button class="btn btn-outline-secondary " type="button" id="togglePassword"><i class="bi bi-eye"></i></button>
+                                            <button class="btn btn-outline-secondary " type="button"
+                                                id="togglePassword"><i class="bi bi-eye"></i></button>
                                         </div>
                                     </div>
                                 </div>
 
                                 <div class="form-group">
-                                    <button type="submit" class="form-control btn btn-primary rounded submit px-3">Sign In</button>
+                                    <button type="submit" class="form-control btn btn-primary rounded submit px-3">Sign
+                                        In</button>
                                 </div>
                                 <div class="form-group d-md-flex">
                                     <div class="w-50 text-left">
@@ -63,7 +74,8 @@ include('header.php');
                                     </div>
                                 </div>
                             </form>
-                            <p class="text-center">Not a member? <a data-toggle="tab" href="membeship-category.php">Register</a></p>
+                            <p class="text-center">Not a member? <a data-toggle="tab"
+                                    href="membeship-category.php">Register</a></p>
                         </div>
                     </div>
                 </div>
@@ -72,14 +84,14 @@ include('header.php');
     </section>
     <?php include('footer.php'); ?>
     <script>
-        document.getElementById("togglePassword").addEventListener("click", function() {
-            var passwordInput = document.getElementById("password");
-            if (passwordInput.type === "password") {
-                passwordInput.type = "text";
-            } else {
-                passwordInput.type = "password";
-            }
-        });
+    document.getElementById("togglePassword").addEventListener("click", function() {
+        var passwordInput = document.getElementById("password");
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text";
+        } else {
+            passwordInput.type = "password";
+        }
+    });
     </script>
 
     <script src="js/jquery.min.js"></script>
