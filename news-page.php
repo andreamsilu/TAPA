b03b059adb9045bb898755ca149634e9bc5f987d
Andrea Msilu <msiluandrew2020@gmail.com>
2025-02-16 11:35:40 +0300

    fixed contact

--- news-page.php
+++ news-page.php
@@ -25,7 +25,11 @@
 <body>
   <!-- ======= Header ======= -->
   <!-- <header id="header" class="fixed-top d-flex align-items-center"> -->
-  <?php include('header.php') ?>
+  <?php 
+   ini_set('display_errors', 1);
+   ini_set('display_startup_errors', 1);
+   error_reporting(E_ALL);
+  include('header.php') ?>
   <!-- </header> -->
   <!-- End Header -->
   <!-- <main id="main"> -->
