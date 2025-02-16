5ab282c5b7d3a5aa2125b0a1e4e456c1b20122b4
Andrea Msilu <msiluandrew2020@gmail.com>
2025-02-16 11:38:51 +0300

    fixed contact

--- news-page.php
+++ news-page.php
@@ -23,64 +23,33 @@
 </head>
 
 <body>
-  <!-- ======= Header ======= -->
-  <!-- <header id="header" class="fixed-top d-flex align-items-center"> -->
   <?php 
-   ini_set('display_errors', 1);
-   ini_set('display_startup_errors', 1);
-   error_reporting(E_ALL);
-  include('header.php') ?>
-  <!-- </header> -->
-  <!-- End Header -->
-  <!-- <main id="main"> -->
-  <!-- ======= Blog Section ======= -->
-  <section class="hero-section inner-page">
-    <!-- <div class="wave">
-        <svg width="1920px" height="265px" viewBox="0 0 1920 265" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-          <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-            <g id="Apple-TV" transform="translate(0.000000, -402.000000)" fill="#FFFFFF">
-              <path d="M0,439.134243 C175.04074,464.89273 327.944386,477.771974 458.710937,477.771974 C654.860765,477.771974 870.645295,442.632362 1205.9828,410.192501 C1429.54114,388.565926 1667.54687,411.092417 1920,477.771974 L1920,667 L1017.15166,667 L0,667 L0,439.134243 Z" id="Path"></path>
-            </g>
-          </g>
-        </svg>
-      </div> -->
-    <!-- <div class="section-title">
-      <h2 class="pt-1">MEMBERSHIP IN TAPA</h2>
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    include('header.php');
+  ?>
+
+  <section class="section">
+    <div class="section-title">
+      <h2 class="pt-1"> TAPA NEWS AND TOPICS</h2>
     </div>
     <div class="container">
-      <div class="row align-items-center">
-        <div class="col-12">
-          <div class="row justify-content-center">
-            <div class="col-md-7 text-center hero-text">
-              <h1 data-aos="fade-up" data-aos-delay="">Blog Posts</h1>
-              <p class="mb-0" data-aos="fade-up" data-aos-delay="100">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section> -->
-    <?php //include "./Admin/news/read_news.php" 
-    ?>
-    <section class="section">
-      <div class="section-title">
-        <h2 class="pt-1"> TAPA NEWS AND TOPICS</h2>
-      </div>
-      <div class="container">
-        <div class="row mb-5">
-          <?php
-      include 'adminpanel/db.php'; 
-      $sql = "SELECT * FROM news WHERE status ='1' ORDER BY date DESC";
-          $result = mysqli_query($conn, $sql);
+      <div class="row mb-5">
+        <?php
+        include 'adminpanel/db.php'; 
+        $sql = "SELECT * FROM news WHERE status ='1' ORDER BY date DESC";
+        $stmt = $conn->prepare($sql);  // Use prepare statement for PDO
+        $stmt->execute();  // Execute the query
 
-           if (mysqli_num_rows($result) > 0) {
-             while ($row = mysqli_fetch_assoc($result)) {
-               $date = $row['date'];
-              // $author = $row['author'];
-              $title = $row['title'];
-              $content = substr($row['description'], 0, 100);
-              $image = $row['image_url'];
-               echo '<div class="col-md-4">
+        // Check if there are any rows returned
+        if ($stmt->rowCount() > 0) {
+          while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $date = $row['date'];
+            $title = $row['title'];
+            $content = substr($row['description'], 0, 100);
+            $image = $row['image_url'];
+            echo '<div class="col-md-4">
                     <div class="post-entry">
                         <a href="Admin/news/full_news.php?id=' . $row['id'] . '" class="d-block mb-4">
                             <img src="adminpanel/' . $image . '" alt="Image" class="img-fluid">
@@ -93,26 +62,27 @@
                         </div>
                     </div>
                 </div>';
-            }
-          } else {
-            echo "No news found!";
           }
-           mysqli_close($conn);
-          ?>
-        </div>
+        } else {
+          echo "No news found!";
+        }
+
+        $stmt = null;  // Close the statement
+        ?>
       </div>
-      <div class="row">
-        <div class="col-12 text-center">
-          <span class="p-3 active text-primary">1</span>
-          <a href="#" class="p-3">2</a>
-          <a href="#" class="p-3">3</a>
-          <a href="#" class="p-3">4</a>
-        </div>
+    </div>
+    <div class="row">
+      <div class="col-12 text-center">
+        <span class="p-3 active text-primary">1</span>
+        <a href="#" class="p-3">2</a>
+        <a href="#" class="p-3">3</a>
+        <a href="#" class="p-3">4</a>
       </div>
-      </div>
-    </section>
-    </main>
-    <!-- End #main -->
-    <!-- ======= Footer ======= -->
-    <?php include('footer.php') ?>
-    
+    </div>
+  </section>
+
+  <?php include('footer.php'); ?>
+
+</body>
+
+</html>
