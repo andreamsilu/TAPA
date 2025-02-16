a2cd9bc4c168cdf8e7ec0d7faa6d598b0978e13c
Andrea Msilu <msiluandrew2020@gmail.com>
2025-02-16 09:04:47 +0300

    fixed contact

--- photo-gallery.php
+++ photo-gallery.php
@@ -1,7 +1,7 @@
 <?php
   error_reporting(E_ALL);
   ini_set('display_errors', 1);
-include "db_connection.php"; // Ensure you have a proper DB connection
+  include 'adminpanel/db.php';  
 ?>
 
 <!DOCTYPE html>
